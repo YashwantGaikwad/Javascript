2aeb5b0b6dd8caa1ebd5018d73024f4ce2cbb002
Yashwant Gaikwad <yashwantg9696@gmail.com>
2025-03-24 13:15:16 +0530

    Initial commit

---
+++ a.js
@@ -0,0 +1,2 @@
+const a = "Yash Gaikwad";
+console.log(a);
---
+++ text.js
@@ -0,0 +1,81 @@
+// const fs = require("fs");
+
+// const contents = fs.readFileSync("a.text","utf-8");
+// console.log(contents);
+// const content = fs.readFileSync("b.text","utf-8");
+// console.log(content);
+
+// function sum(a,b){
+//     return a+b;
+// }
+// function multi(a,b){
+//     return a*b;
+// }
+// function sub(a,b){
+//     return a-b;
+// }
+// function div(a,b){
+//     return a/b;
+// }
+// function doOperation(a,b,op){
+//     return op(a,b);
+// }
+
+// console.log(doOperation(5,5,sum));
+// const ans = sum(4,5);
+// console.log(ans);
+
+
+// function timeout(){
+//     console.log("By....!");
+// }
+
+console.log("Hii..!");
+
+// setTimeout(timeout,1000);
+
+// console.log("Welcome the loop");
+// console.log("Click the Button");
+
+// let c=0;
+// for(i=0;i<1000000000;i++){
+//     c= c + 1;
+// }
+// console.log("Very high code");
+
+// class rectangle{
+//     constructor(width, height, color){
+//         this.width = width;
+//         this.height = height;
+//         this.color = color;
+//     }
+
+//     area(){
+//         const area = this.width*this.height;
+//         return area;
+//     }
+// }
+
+// const rect = new rectangle(5,8);
+// const area = rect.area();
+// console.log(area);
+
+
+// const now = new Date();
+// console.log(now.toISOString());
+
+// const map = new Map();
+// map.set("name", "Yashwant");
+// map.set("age",24);
+
+// console.log(map.get("name"));
+
+
+
+// function random(){
+
+// }
+
+// let p = new Promise(random);
+// console.log(p);
+
